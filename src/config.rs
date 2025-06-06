c58349d0e3c6b7c9e53a122b578d3d9a3c33519d
yaoyue <yaoyue@fjwr.xyz>
2025-06-06 18:37:07 +0800

    修改服务器地址

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rsapi.fjwr.xyz"];
+pub const RS_PUB_KEY: &str = "WGrt5SFUGp4YnT3ksrrEywW8PWsLn7G1ve5p+BirCxk=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
